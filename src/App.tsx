cd5df176c3f58b3ba3758941e2e9048cddd21176
bak-ain <cherryain8452@gmail.com>
2025-06-01 20:38:23 +0900

    setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Home from './pages/home/Home';
+import Login from './pages/auth/Login';
+import Join from './pages/auth/Join';
+import OfficialFeed from './pages/feed/OfficialFeed';
+import ArtistFeed from './pages/feed/ArtistFeed';
+import FanFeed from './pages/feed/FanFeed';
+import Mypage from './pages/user/Mypage';
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/join" element={<Join />} />
+      <Route path="/officialfeed" element={<OfficialFeed />} />
+      <Route path="/artistfeed" element={<ArtistFeed />} />
+      <Route path="/fanfeed" element={<FanFeed />} />
+      <Route path="/mypage" element={<Mypage />} />
+    </Routes>
   );
 }
 
