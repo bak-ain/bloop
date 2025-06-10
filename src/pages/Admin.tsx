75360a1e790389a4cedc6ad4269b606ce1458cf6
kimjeongg <rlawjddl3916@gmail.com>
2025-06-10 09:35:59 +0900

    로컬 변경 내용 커밋

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -2,7 +2,7 @@ import styles from './Admin.module.css';
 
 const Admin = () => {
     return (
-        <div  className={`${styles.wrap} inner`}>
+        <div  className={`${styles.wrap} inner h3`}>
             Admin
         </div>
     )
